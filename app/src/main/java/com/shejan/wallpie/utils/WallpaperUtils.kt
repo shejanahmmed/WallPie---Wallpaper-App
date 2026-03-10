59183b55f55daa91ce466f951dc38ab4c8e5a40d
Shejan <beingshejanahmmed@gmail.com>
2026-03-11 03:00:11 +0600

    feat(wallpaper): center crop wallpaper to fit screen dimensions

--- app/src/main/java/com/shejan/wallpie/utils/WallpaperUtils.kt
+++ app/src/main/java/com/shejan/wallpie/utils/WallpaperUtils.kt
@@ -12,6 +12,8 @@ import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
+import android.util.DisplayMetrics
+import android.view.WindowManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -28,12 +30,39 @@ object WallpaperUtils {
                     val wallpaperManager = WallpaperManager.getInstance(context)
                     CoroutineScope(Dispatchers.IO).launch {
                         try {
+                            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+                            val metrics = DisplayMetrics()
+                            @Suppress("DEPRECATION")
+                            windowManager.defaultDisplay.getRealMetrics(metrics)
+                            
+                            val screenWidth = metrics.widthPixels
+                            val screenHeight = metrics.heightPixels
+
+                            val bitmapRatio = resource.width.toFloat() / resource.height.toFloat()
+                            val screenRatio = screenWidth.toFloat() / screenHeight.toFloat()
+
+                            val newWidth: Int
+                            val newHeight: Int
+                            if (bitmapRatio > screenRatio) {
+                                newHeight = screenHeight
+                                newWidth = (newHeight * bitmapRatio).toInt()
+                            } else {
+                                newWidth = screenWidth
+                                newHeight = (newWidth / bitmapRatio).toInt()
+                            }
+
+                            val scaledBitmap = Bitmap.createScaledBitmap(resource, newWidth, newHeight, true)
+                            val xOffset = (newWidth - screenWidth) / 2
+                            val yOffset = (newHeight - screenHeight) / 2
+
+                            val croppedBitmap = Bitmap.createBitmap(scaledBitmap, xOffset, yOffset, screenWidth, screenHeight)
+
                             when (type) {
-                                WallpaperType.HOME -> wallpaperManager.setBitmap(resource, null, true, WallpaperManager.FLAG_SYSTEM)
-                                WallpaperType.LOCK -> wallpaperManager.setBitmap(resource, null, true, WallpaperManager.FLAG_LOCK)
+                                WallpaperType.HOME -> wallpaperManager.setBitmap(croppedBitmap, null, true, WallpaperManager.FLAG_SYSTEM)
+                                WallpaperType.LOCK -> wallpaperManager.setBitmap(croppedBitmap, null, true, WallpaperManager.FLAG_LOCK)
                                 WallpaperType.BOTH -> {
-                                    wallpaperManager.setBitmap(resource, null, true, WallpaperManager.FLAG_SYSTEM)
-                                    wallpaperManager.setBitmap(resource, null, true, WallpaperManager.FLAG_LOCK)
+                                    wallpaperManager.setBitmap(croppedBitmap, null, true, WallpaperManager.FLAG_SYSTEM)
+                                    wallpaperManager.setBitmap(croppedBitmap, null, true, WallpaperManager.FLAG_LOCK)
                                 }
                             }
                             withContext(Dispatchers.Main) {
